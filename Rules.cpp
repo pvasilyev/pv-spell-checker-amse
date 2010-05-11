a76661b99daac0f6e12082d9cec5c6268f2f47d5
vasiliev.paul <vasiliev.paul@ffdc70ac-19a7-11df-8728-136f2c2ca76e>
2010-05-11 23:28:31 +0000

    fix rules

--- Rules.cpp
+++ Rules.cpp
@@ -2,16 +2,21 @@
 #include "SyntaxAnalyzer.h"
 #include "VLemmatizer.h"
 
+std::vector<WordDescription::Grammem> Rule::getGrammems() const {
+	std::vector<WordDescription::Grammem> vector;
+	vector.push_back(WordDescription::PLURAL);
+	vector.push_back(WordDescription::SINGULAR);
+	vector.push_back(WordDescription::MASCULINUM);
+	vector.push_back(WordDescription::FEMINUM);
+	vector.push_back(WordDescription::NEUTRUM);
+	return vector;
+}
+
 bool SubjectCoordinationRule::accepts(const std::vector<SentencePart> &subjects) const {
 	if (!subjects.empty()) {
 		const WordDescription &firstSubject = subjects.at(0).myWordDescription;
 
-		std::vector<WordDescription::Grammem> vectorGrammems;
-		vectorGrammems.push_back(WordDescription::PLURAL);
-		vectorGrammems.push_back(WordDescription::SINGULAR);
-		vectorGrammems.push_back(WordDescription::MASCULINUM);
-		vectorGrammems.push_back(WordDescription::FEMINUM);
-		vectorGrammems.push_back(WordDescription::NEUTRUM);
+		std::vector<WordDescription::Grammem> vectorGrammems = this->getGrammems();
 
 		std::vector<WordDescription::PartOfSpeech> vectorPartsOfSpeech;
 		vectorPartsOfSpeech.push_back(WordDescription::NOUN);
@@ -40,12 +45,7 @@ bool PredicateCoordinationRule::accepts(const std::vector<SentencePart> &predica
 	if (!predicates.empty()) {
 		const WordDescription &firstPredicate = predicates.at(0).myWordDescription;
 
-		std::vector<WordDescription::Grammem> vectorGrammems;
-		vectorGrammems.push_back(WordDescription::PLURAL);
-		vectorGrammems.push_back(WordDescription::SINGULAR);
-		vectorGrammems.push_back(WordDescription::MASCULINUM);
-		vectorGrammems.push_back(WordDescription::FEMINUM);
-		vectorGrammems.push_back(WordDescription::NEUTRUM);
+		std::vector<WordDescription::Grammem> vectorGrammems = this->getGrammems();
 		vectorGrammems.push_back(WordDescription::PASTTENSE);
 		vectorGrammems.push_back(WordDescription::PRESENTTENSE);
 		vectorGrammems.push_back(WordDescription::FUTURETENSE);
